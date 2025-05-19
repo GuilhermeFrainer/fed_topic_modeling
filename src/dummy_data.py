06069171906b6169001791fd1ab3a1e5e63f99e8
Guilherme Frainer <gui.sf.2012@gmail.com>
2025-05-19 16:53:18 -0300

    Improved dummy data code slightly

--- src/dummy_data.py
+++ src/dummy_data.py
@@ -1,11 +1,20 @@
 from sklearn.datasets import fetch_20newsgroups
 from nltk.corpus import stopwords
+from dotenv import dotenv_values
 import polars as pl
 import nltk
 import preprocessing
+import pathlib
+
+
+ROWS = 10
+FILE = "dummy.csv"
 
 
 def main():
+    config = dotenv_values(".env")
+    data_dir = pathlib.Path(config["DATA_DIR"])
+
     stop_words = set(stopwords.words("english"))
     stemmer = nltk.stem.SnowballStemmer("english")
 
@@ -14,12 +23,14 @@ def main():
         remove=("headers", "footers", "quotes"),
         categories=["sci.space", "rec.sport.baseball"])
 
-    sample = data["data"][:10]
+    sample = data["data"][:ROWS]
     preprocessed_text = [preprocessing.preprocess(s, stop_words=stop_words) for s in sample]
     stemmed_text = [preprocessing.stem(s, stemmer=stemmer) for s in preprocessed_text]
 
     df = pl.DataFrame({"raw_text": sample, "clean_text": preprocessed_text, "stemmed_text": stemmed_text})
-    df.write_csv("../data/dummy_data.csv")
+    filename = data_dir / FILE
+    df.write_csv(filename)
+    print("Dummy data file saved to " + str(filename))
 
 
 if __name__ == "__main__":
